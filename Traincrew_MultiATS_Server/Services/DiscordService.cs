89cdb8a40a0df4247e353d299ce09bfcd8b819f5
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2024-11-17 15:38:21 +0900

    add忘れ

--- Traincrew_MultiATS_Server/Services/DiscordService.cs
+++ Traincrew_MultiATS_Server/Services/DiscordService.cs
@@ -1,12 +1,41 @@
 using Discord;
 using Discord.Rest;
+using Discord.WebSocket;
 using Traincrew_MultiATS_Server.Exception.DiscordAuthenticationException;
 using Traincrew_MultiATS_Server.Models;
 
 namespace Traincrew_MultiATS_Server.Services;
 
-public class DiscordService(IConfiguration configuration)
+public class DiscordService
 {
+    private readonly IConfiguration configuration;
+    private readonly DiscordSocketClient client;
+    private bool isStarted;
+    private readonly TaskCompletionSource isReady = new();
+
+    public DiscordService(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+        client = new DiscordSocketClient();
+    }
+
+    private async Task Initialize()
+    {
+        if (!isStarted)
+        {
+            var token = configuration.GetSection("Discord:BotToken").Get<string>();
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+            isStarted = true;
+        }
+        client.Ready += () =>
+        {
+            isReady.SetResult();
+            return Task.CompletedTask;
+        };
+        await isReady.Task;
+    }
+
     public async Task<(RestGuildUser, TraincrewRole)> DiscordAuthentication(string token)
     {
         var guildId = configuration.GetValue<ulong>("Discord:GuildId");
@@ -27,17 +56,35 @@ public class DiscordService(IConfiguration configuration)
             throw new DiscordAuthenticationException("You don't have the required role.");
         }
 
-        return (member, GetRole(member));
+        return (member, GetRole(member.RoleIds));
     }
 
-    private TraincrewRole GetRole(RestGuildUser member)
+    public async Task<TraincrewRole> GetRoleByMemberId(ulong memberId)
     {
-        var roleIds = new HashSet<ulong>(member.RoleIds);
+        // Todo: 本来はサーバー起動時にするべき
+        await Initialize().WaitAsync(TimeSpan.FromSeconds(1));
+        var guildId = configuration.GetValue<ulong>("Discord:GuildId");
+        var guild = client.GetGuild(guildId);
+        if (guild is null)
+        {
+            throw new DiscordAuthenticationException("The server is not found.");
+        }
+        var member = guild.GetUser(memberId);
+        if (member is null)
+        {
+            throw new DiscordAuthenticationException("The member is not found.");
+        } 
+        return GetRole(member.Roles.Select(x => x.Id).ToArray());
+    }
+
+    private TraincrewRole GetRole(IReadOnlyCollection<ulong> roleIds)
+    {
+        var roleIdSet = new HashSet<ulong>(roleIds);
         var result = new TraincrewRole
         {
-            IsDriver = configuration.GetSection("Discord:Roles:Driver").Get<ulong[]>().Any(roleIds.Contains),
-            IsCommander = configuration.GetSection("Discord:Roles:Commander").Get<ulong[]>().Any(roleIds.Contains),
-            IsSignalman = configuration.GetSection("Discord:Roles:Signalman").Get<ulong[]>().Any(roleIds.Contains)
+            IsDriver = configuration.GetSection("Discord:Roles:Driver").Get<ulong[]>().Any(roleIdSet.Contains),
+            IsCommander = configuration.GetSection("Discord:Roles:Commander").Get<ulong[]>().Any(roleIdSet.Contains),
+            IsSignalman = configuration.GetSection("Discord:Roles:Signalman").Get<ulong[]>().Any(roleIdSet.Contains)
         };
         return result;
     }
