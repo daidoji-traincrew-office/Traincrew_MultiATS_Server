b8bd375140e0dd07c70c2fa25a62bce8839e7782
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2024-12-13 23:37:29 +0900

    Route にDescriptionなんざ必要ねぇんだよ！

--- Traincrew_MultiATS_Server/Models/Route.cs
+++ Traincrew_MultiATS_Server/Models/Route.cs
@@ -6,7 +6,6 @@ namespace Traincrew_MultiATS_Server.Models;
 public class Route : InterlockingObject
 {
     public string TcName { get; set; }
-    public string? Description { get; set; }
     public string RouteType { get; set; }
     public string? Root { get; set; }
     public string? Indicator { get; set; }
