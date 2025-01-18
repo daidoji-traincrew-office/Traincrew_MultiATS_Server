db5406145e3dfb1b6f5fc0c57197e217b2a55b05
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-01-18 20:34:23 +0900

    手直し

--- Traincrew_MultiATS_Server/Services/TrackCircuitService.cs
+++ Traincrew_MultiATS_Server/Services/TrackCircuitService.cs
@@ -4,63 +4,57 @@ namespace Traincrew_MultiATS_Server.Services;
 
 public class TrackCircuitService(ITrackCircuitRepository trackCircuitRepository)
 {
-	public async Task<List<TrackCircuitData>> GetAllTrackCircuitDataList()
-	{
-		List<Models.TrackCircuit> trackCircuits_db = await trackCircuitRepository.GetAllTrackCircuitList();;
-		List<TrackCircuitData> trackCircuitDataList = 
-			trackCircuits_db.Select(item => new TrackCircuitData()
-			{
-				Last = item.TrackCircuitState.TrainNumber,
-				Name = item.Name,
-				On = item.TrackCircuitState.IsShortCircuit
-			}).ToList();
-		return trackCircuitDataList;
-	}
-	public async Task<List<TrackCircuitData>> GetTrackCircuitDataListByTrainNumber(string trainNumber)
-	{
-		List<Models.TrackCircuit> trackCircuits_db = await trackCircuitRepository.GetTrackCircuitListByTrainNumber(trainNumber);
-		List<TrackCircuitData> trackCircuitDataList = 
-			trackCircuits_db.Select(item => new TrackCircuitData()
-			{
-				Last = item.TrackCircuitState.TrainNumber,
-				Name = item.Name,
-				On = item.TrackCircuitState.IsShortCircuit
-			}).ToList();
-		return trackCircuitDataList;
-	}
+    public async Task<List<TrackCircuitData>> GetAllTrackCircuitDataList()
+    {
+        List<Models.TrackCircuit> trackCircuitsDb = await trackCircuitRepository.GetAllTrackCircuitList();
+        List<TrackCircuitData> trackCircuitDataList = trackCircuitsDb
+            .Select(ToTrackCircuitData)
+            .ToList();
+        return trackCircuitDataList;
+    }
 
-	public async Task SetTrackCircuitDataList(List<TrackCircuitData> trackCircuitData, string trainNumber)
-	{
-		List<Models.TrackCircuit> trackCircuit = new List<Models.TrackCircuit>();
-		foreach (var item in trackCircuitData)
-		{
-			trackCircuit.Add(new Models.TrackCircuit()
-			{
-				Name = item.Name,
-				TrackCircuitState = new Models.TrackCircuitState()
-				{
-					TrainNumber = trainNumber,
-					IsShortCircuit = true
-				}
-			});
-		}
-		await trackCircuitRepository.SetTrackCircuitList(trackCircuit, trainNumber);
-	}
-	public async Task ClearTrackCircuitDataList(List<TrackCircuitData> trackCircuitData)
-	{
-		List<Models.TrackCircuit> trackCircuit = new List<Models.TrackCircuit>();
-		foreach (var item in trackCircuitData)
-		{
-			trackCircuit.Add(new Models.TrackCircuit()
-			{
-				Name = item.Name,
-				TrackCircuitState = new Models.TrackCircuitState()
-				{
-					TrainNumber = "",
-					IsShortCircuit = false
-				}
-			});
-		}
-		await trackCircuitRepository.ClearTrackCircuitList(trackCircuit);
-	}
+    public async Task<List<TrackCircuitData>> GetTrackCircuitDataListByTrainNumber(string trainNumber)
+    {
+        List<Models.TrackCircuit> trackCircuitsDb =
+            await trackCircuitRepository.GetTrackCircuitListByTrainNumber(trainNumber);
+        List<TrackCircuitData> trackCircuitDataList = trackCircuitsDb
+            .Select(ToTrackCircuitData)
+            .ToList();
+        return trackCircuitDataList;
+    }
+
+    public async Task SetTrackCircuitDataList(List<TrackCircuitData> trackCircuitData, string trainNumber)
+    {
+        List<Models.TrackCircuit> trackCircuit = trackCircuitData
+            .Select(item => new Models.TrackCircuit
+            {
+                Name = item.Name,
+                TrackCircuitState = new() { TrainNumber = trainNumber, IsShortCircuit = true }
+            })
+            .ToList();
+        await trackCircuitRepository.SetTrackCircuitList(trackCircuit, trainNumber);
+    }
+
+    public async Task ClearTrackCircuitDataList(List<TrackCircuitData> trackCircuitData)
+    {
+        List<Models.TrackCircuit> trackCircuit = trackCircuitData
+            .Select(item => new Models.TrackCircuit
+            {
+                Name = item.Name,
+                TrackCircuitState = new() { TrainNumber = "", IsShortCircuit = false }
+            })
+            .ToList();
+
+        await trackCircuitRepository.ClearTrackCircuitList(trackCircuit);
+    }
+
+    private static TrackCircuitData ToTrackCircuitData(Models.TrackCircuit trackCircuit)
+    {
+        return new()
+        {
+            Last = trackCircuit.TrackCircuitState.TrainNumber,
+            Name = trackCircuit.Name,
+            On = trackCircuit.TrackCircuitState.IsShortCircuit
+        };
+    }
 }
