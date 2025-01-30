44056e8ed83225691836bf0a5fb1f8cddaeebe81
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-01-31 02:14:48 +0900

    Me Endpointを認証情報判断に使えるように修正

--- Traincrew_MultiATS_Server/Controllers/MeController.cs
+++ Traincrew_MultiATS_Server/Controllers/MeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OpenIddict.Validation.AspNetCore;
+using Traincrew_MultiATS_Server.Models;
 using Traincrew_MultiATS_Server.Services;
 using static OpenIddict.Abstractions.OpenIddictConstants;
 
@@ -14,8 +15,30 @@ public class MeController(DiscordService discordService) : ControllerBase
     [Authorize(AuthenticationSchemes = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme)]
     public async Task<IActionResult> GetMe()
     {
-        // Todo: memberIDこれであってる？
-        var memberId = ulong.Parse(User.FindFirst(Claims.Subject)!.Value);
+        // 認可が有効の場合に、トークンなしでアクセスした場合、403が返される
+        // この処理が実行されるのは、認可が有効でトークンあり または、認可が無効の場合(ローカルデバッグ用)
+        
+        // MemberIDを取得
+        var memberIdString = User.FindFirst(Claims.Subject)?.Value;
+        // 取得できなかった場合、ローカルデバッグモード用の情報を返す
+        if(memberIdString == null)
+        {
+            return Ok(new
+            {
+                Name = "ローカルデバッグモード",
+                Role = new TraincrewRole
+                {
+                    IsDriver = true,
+                    IsDriverManager = true,
+                    IsConductor = true,
+                    IsCommander = true,
+                    IsSignalman = true,
+                    IsAdministrator = true
+                } 
+            });
+        }
+        // 取得できた場合、MemberIDを取得し、DiscordServiceを使用してRoleを取得
+        var memberId = ulong.Parse(memberIdString);
         var role = await discordService.GetRoleByMemberId(memberId);
         return Ok(new
         {
