bfda49223a92154872d9c0b9973998820de3715f
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-02-02 17:50:44 +0900

    防護無線の発砲の処理をしっかり更新するように変更

--- Traincrew_MultiATS_Server/Repositories/Protection/ProtectionRepository.cs
+++ Traincrew_MultiATS_Server/Repositories/Protection/ProtectionRepository.cs
@@ -9,19 +9,49 @@ public class ProtectionRepository(ApplicationDbContext context) : IProtectionRep
     public async Task<bool> IsProtectionEnabled(int minProtectionZone, int maxProtectionZone)
     {
         return await context.protectionZoneStates
-            .Where(x => minProtectionZone <=  x.ProtectionZone && x.ProtectionZone <= maxProtectionZone)
+            .Where(x => minProtectionZone <= x.ProtectionZone && x.ProtectionZone <= maxProtectionZone)
             .AnyAsync();
     }
 
     public async Task EnableProtection(string trainNumber, List<int> protectionZones)
     {
-        context.protectionZoneStates.AddRange(protectionZones
-            .Select(protectionZone => new ProtectionZoneState
-            {
-                TrainNumber = trainNumber,
-                ProtectionZone = protectionZone
-            }));
+        // トランザクション内で処理する(追加削除は同時操作としたいため)
+        await using var transaction = await context.Database.BeginTransactionAsync();
+        // 既存のエントリを取得
+        // Entityにしてるのは、削除時にEntityをそのまま渡すため
+        // 何も考えずRemoveRangeにしてると、既存のTrackingしているEntityがいた時にエラーが出る
+        var oldEntities = await context.protectionZoneStates
+            .Where(x => x.TrainNumber == trainNumber)
+            .ToListAsync();
+        // 既存のProtectionZoneを取得
+        var oldZones = oldEntities.Select(x => x.ProtectionZone).ToList();
+        
+        // 追加、削除するProtectionZoneを取得
+        var zonesToAdd = protectionZones.Except(oldZones).ToList();
+        var zonesToRemove = oldZones.Except(protectionZones).ToList();
+
+        // 追加するProtectionZoneがあれば追加
+        if (zonesToAdd.Count != 0)
+        {
+            context.protectionZoneStates.AddRange(zonesToAdd
+                .Select(protectionZone => new ProtectionZoneState
+                {
+                    TrainNumber = trainNumber,
+                    ProtectionZone = protectionZone
+                }));
+        }
+
+        // 削除するProtectionZoneがあれば削除
+        if (zonesToRemove.Count != 0)
+        {
+            context.protectionZoneStates.RemoveRange(
+                oldEntities.Where(x => zonesToRemove.Contains(x.ProtectionZone))
+            );
+        }
+        
+        // 保存
         await context.SaveChangesAsync();
+        await transaction.CommitAsync();
     }
 
     public async Task DisableProtection(string trainNumber)
