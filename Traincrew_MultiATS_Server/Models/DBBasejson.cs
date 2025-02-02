092cd430ee8ab5f918addfa30cc6c4fda6255aba
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-02-02 18:02:10 +0900

    DBBasejsonの定義を更新

--- Traincrew_MultiATS_Server/Models/DBBasejson.cs
+++ Traincrew_MultiATS_Server/Models/DBBasejson.cs
@@ -2,8 +2,9 @@ namespace Traincrew_MultiATS_Server.Models;
 
 public class JsonTrackCircuitData : TrackCircuitData
 {
-    public List<string>? NextSignalNamesUp { get; init; } = null;
-    public List<string>? NextSignalNamesDown { get; init; } = null;
+    public int ProtectionZone { get; init; }
+    public List<string> NextSignalNamesUp { get; init; } = [];
+    public List<string> NextSignalNamesDown { get; init; } = [];
 }
 
 public class JsonSignalData: SignalData
@@ -18,5 +19,4 @@ public class DBBasejson
     public List<JsonTrackCircuitData> trackCircuitList {get; set;}
     public List<JsonSignalData> signalDataList {get; set;}
     public List<SignalTypeData> signalTypeList { get; set; }
-    public List<ProtectionZoneState> protectionZoneStateList {get; set;}
 }
