8dbfc77356545af32aca72b95c672740abcfe3bb
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-02-08 20:23:40 +0900

    station_interlocking objectを作成

---
+++ Traincrew_MultiATS_Server/Models/StationInterlockingObject.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Traincrew_MultiATS_Server.Models;
+
+[Table("station_interlocking_object")]
+public class StationInterlockingObject
+{
+    [Key]
+    [Column(Order = 0)]
+    public required string StationId { get; set; }
+
+    [Key]
+    [Column(Order = 1)]
+    public required ulong ObjectId { get; set; }
+
+    [ForeignKey("StationId")]
+    public virtual Station Station { get; set; }
+
+    [ForeignKey("ObjectId")]
+    public virtual InterlockingObject InterlockingObject { get; set; }
+}
