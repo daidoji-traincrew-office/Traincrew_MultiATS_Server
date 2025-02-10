a35440d27149f713dd67901289b3e03a50b4dec2
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-02-10 23:35:08 +0900

    Add忘れ

---
+++ Traincrew_MultiATS_Server/Repositories/SwitchingMachineRoute/ISwitchingMachineRouteRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Traincrew_MultiATS_Server.Repositories.SwitchingMachineRoute;
+
+public interface ISwitchingMachineRouteRepository
+{
+    Task<List<Models.SwitchingMachineRoute>> GetAll();
+}
