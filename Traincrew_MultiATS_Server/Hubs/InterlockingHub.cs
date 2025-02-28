07bb8b647963ab0b8fbfa84a6288a5fe83e3b893
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-03-01 03:06:04 +0900

    TOど

--- Traincrew_MultiATS_Server/Hubs/InterlockingHub.cs
+++ Traincrew_MultiATS_Server/Hubs/InterlockingHub.cs
@@ -14,40 +14,49 @@ public class InterlockingHub(
 {
     public async Task<DataToInterlocking> SendData_Interlocking(ConstantDataFromInterlocking clientData)
     {
-        DataToInterlocking response = new DataToInterlocking();
-        response.TrackCircuits = await trackCircuitService.GetAllTrackCircuitDataList();
-
-        // Todo: TraincrewRole Authenticationsを設定する(role認証がどうにかなったあたりでつなぎこむ)
-        // response.Authentications =                       
-
-        // Todo: List<InterlockingSwitchData> Pointsを設定する
-        // response.Points =                              
-
+        // Todo: めんどいし、Interlocking全取得してOfTypeと変換作って動くようにする
+        List<InterlockingObject> interlockingObjects = [];
+        List<DestinationButton> destinationButtons = [];
         // List<string> clientData.ActiveStationsListの駅IDから、指定された駅にある信号機名称をList<string>で返すやつ
         var stationNames = await stationService.GetStationNamesByIds(clientData.ActiveStationsList);
         var signalNames = await signalService.GetSignalNamesByStationNames(stationNames);
         // それら全部の信号の現示計算
         var signalIndications = await signalService.CalcSignalIndication(signalNames);
-        response.Signals = signalIndications.Select(pair => new SignalData
+        
+        var response = new DataToInterlocking
         {
-            Name = pair.Key,
-            phase = pair.Value
-        }).ToList();
+            TrackCircuits = await trackCircuitService.GetAllTrackCircuitDataList(),
+            // Todo: TraincrewRole Authenticationsを設定する(role認証がどうにかなったあたりでつなぎこむ)
+            // Authentications =                       
+            Points = interlockingObjects
+                .OfType<SwitchingMachine>()
+                .Select(SwitchingMachineService.ToInterlockingSwitchData)
+                .ToList(),
+            // Todo: List<InterlockingLeverData> PhysicalLeversを設定する
+            // Todo: 変換実装と、方向てこ以外のてこにフィルターする
+            PhysicalLevers = interlockingObjects
+                .OfType<Lever>()
+                .Select(lever => new InterlockingLeverData())
+                .ToList(),
+            // Todo: List<DestinationButtonState> PhysicalButtonsを設定する
+            // Todo: こいつはまずレスポンス定義するところから
+            // PhysicalButtons = [] 
 
-        // Todo: List<InterlockingLeverData> PhysicalLeversを設定する
-        // response.PhysicalLevers =                           
+            // Todo: List<InterlockingDirectionData> Directionsを設定する
+            // Todo: 方向てこ実装、方向てこにフィルターする
+            Directions = [],
 
-        // Todo: List<DestinationButtonState> PhysicalButtonsを設定する
-        // response.PhysicalButtons =                        
+            // Todo: List<InterlockingRetsubanData> Retsubansを設定する
+            // Todo: 列番表示の実装から
+            // Retsubans =                              
 
-        // Todo: List<InterlockingDirectionData> Directionsを設定する
-        // response.PhysicalButtons =                          
-
-        // Todo: List<InterlockingRetsubanData> Retsubansを設定する
-        // response.Retsubans =                              
-
-        // Todo: List<Dictionary<string, bool>> Lampsを設定する
-        // response.Lamps = 
+            // Todo: List<Dictionary<string, bool>> Lampsを設定する
+            // Todo: これは何を設定すればええんや・・・？
+            // response.Lamps = 
+            Signals = signalIndications
+                .Select(pair => SignalService.ToSignalData(pair.Key, pair.Value))
+                .ToList()
+        };
         return response;
     }
 }
