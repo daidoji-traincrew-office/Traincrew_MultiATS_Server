50b1f3968679aadaaa597436e390914d695807fd
TakumiteTudo <takumit2886@gmail.com>
2025-02-28 20:22:27 +0900

    現行仕様へ調整、Todo書込み

--- Traincrew_MultiATS_Server/Hubs/InterlockingHub.cs
+++ Traincrew_MultiATS_Server/Hubs/InterlockingHub.cs
@@ -9,10 +9,34 @@ namespace Traincrew_MultiATS_Server.Hubs;
 [Authorize(AuthenticationSchemes = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme)]
 public class InterlockingHub(TrackCircuitService trackCircuitService) : Hub
 {
-	public async Task<Models.DataToInterlocking> SendData_TID(Models.DataFromInterlocking dataFromInterlocking)
-	{
-		Models.DataToInterlocking response = new Models.DataToInterlocking();
-		response.TrackCircuitDataList = await trackCircuitService.GetAllTrackCircuitDataList();
-		return response;
-	}
+    public async Task<Models.DataToInterlocking> SendData_Interlocking(Models.DataToInterlocking dataToInterlocking)
+    {
+        Models.DataToInterlocking response = new Models.DataToInterlocking();
+        response.TrackCircuits = await trackCircuitService.GetAllTrackCircuitDataList();
+
+        // Todo: TraincrewRole Authentications‚ğİ’è‚·‚é
+        // response.Authentications =                       
+
+        // Todo: List<InterlockingSwitchData> Points‚ğİ’è‚·‚é
+        // response.Points =                              
+
+        // Todo: List<InterlockingSignalData> Signals‚ğİ’è‚·‚é
+        // response.Signals =                               
+
+        // Todo: List<InterlockingLeverData> PhysicalLevers‚ğİ’è‚·‚é
+        // response.PhysicalLevers =                           
+
+        // Todo: List<DestinationButtonState> PhysicalButtons‚ğİ’è‚·‚é
+        // response.PhysicalButtons =                        
+
+        // Todo: List<InterlockingDirectionData> Directions‚ğİ’è‚·‚é
+        // response.PhysicalButtons =                          
+
+        // Todo: List<InterlockingRetsubanData> Retsubans‚ğİ’è‚·‚é
+        // response.Retsubans =                              
+
+        // Todo: List<Dictionary<string, bool>> Lamps‚ğİ’è‚·‚é
+        // response.Lamps = 
+        return response;
+    }
 }
