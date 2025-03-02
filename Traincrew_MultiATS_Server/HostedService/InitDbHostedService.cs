b2aa537428c9310440d70e683e045acb6a40928c
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-03-02 19:41:48 +0900

    館浜の連動図表を読み込み

--- Traincrew_MultiATS_Server/HostedService/InitDbHostedService.cs
+++ Traincrew_MultiATS_Server/HostedService/InitDbHostedService.cs
@@ -685,6 +685,7 @@ internal partial class DbRendoTableInitializer
         // 進路とてこと着点ボタンの関連付けを追加
         foreach (var (route, leverId, buttonName) in routes)
         {
+            // Todo: 単線区間の進路は、着点がないことに注意
             context.RouteLeverDestinationButtons.Add(new()
             {
                 RouteId = route.Id,
