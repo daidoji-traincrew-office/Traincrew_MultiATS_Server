2d34ba04eec00a504128ed3492e404650f6162be
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-03-02 15:49:37 +0900

    不要コード削除

--- Traincrew_MultiATS_Server/Services/SwitchingMachineService.cs
+++ Traincrew_MultiATS_Server/Services/SwitchingMachineService.cs
@@ -18,36 +18,6 @@ public class SwitchingMachineService(
     IGeneralRepository generalRepository
     )
 {
-    private const int SwitchingTime = 3;
-    public async Task SetSwitchingMachines(List<(ulong, NR)> switchingMachineList)
-    {
-        var now = dateTimeRepository.GetNow();
-        var switchingMachineIds = switchingMachineList.Select(x => x.Item1);
-        var switchingMachines = await switchingMachineRepository
-            .GetSwitchingMachinesByIdsWithState(switchingMachineIds);
-        var switchingMachineDic = switchingMachines.ToDictionary(x => x.Id);
-        // Todo: 鎖状状態の取得と確認
-        
-        foreach (var (id, isReverse) in switchingMachineList)
-        {
-            if (!switchingMachineDic.TryGetValue(id, out var switchingMachine))
-            {
-                // Todo: 存在しないIDなので例外
-                continue;
-            }
-            if (switchingMachine.SwitchingMachineState.SwitchEndTime > now)
-            {
-                // Todo: 転換中なので例外
-            }
-            /*
-            if (switchingMachine.SwitchingMachineState.IsReverse == isReverse)
-            {
-                continue;
-            }
-            */
-        }
-    }
-    
     /// <summary>
     /// <strong>転てつ器制御回路・転てつ器表示リレー回路</strong><br/>
     /// 現在の状態から、転てつ器の状態を決定する。表示リレー回路直接は存在せず、IsSwitchingとIsReverseの組み合わせを毎度取得する。
