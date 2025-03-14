b4036a99fdecc4f236f5e88fa273b72caba665d1
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-03-15 01:33:35 +0900

    TrackCircuitStateに補正リレーの状態とタイムスタンプを追加(CSharp)

--- Traincrew_MultiATS_Server/Models/TrackCircuitState.cs
+++ Traincrew_MultiATS_Server/Models/TrackCircuitState.cs
@@ -9,4 +9,10 @@ public class TrackCircuitState
     public required string TrainNumber { get; set; }
     public bool IsShortCircuit { get; set; }
     public bool IsLocked { get; set; }
+    public RaiseDrop IsCorrectionRaiseRelayRaised { get; set; }
+    [Column(TypeName = "timestamp without time zone")]
+    public DateTime? RaisedAt { get; set; }
+    public RaiseDrop IsCorrectionDropRelayRaised { get; set; }
+    [Column(TypeName = "timestamp without time zone")]
+    public DateTime? DroppedAt { get; set; }
 }
