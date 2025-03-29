338ed7edd00e6d56819de9d7c3386f79ee128781
TakumiteTudo <takumit2886@gmail.com>
2025-03-30 02:17:08 +0900

    一部summary追加

--- Traincrew_MultiATS_Server/Models/TrackCircuitState.cs
+++ Traincrew_MultiATS_Server/Models/TrackCircuitState.cs
@@ -7,11 +7,23 @@ public class TrackCircuitState
 {
     public ulong Id { get; init; }
     public required string TrainNumber { get; set; }
+    /// <summary>
+    /// 親TR相当
+    /// RaiseDropではない
+    /// </summary>
     public bool IsShortCircuit { get; set; }
     public bool IsLocked { get; set; }
+    /// <summary>
+    /// 不正扛上補正済みリレー
+    /// F付リレー
+    /// </summary>
     public RaiseDrop IsCorrectionRaiseRelayRaised { get; set; }
     [Column(TypeName = "timestamp without time zone")]
     public DateTime? RaisedAt { get; set; }
+    /// <summary>
+    /// 不正落下補正済みリレー
+    /// B付リレー
+    /// </summary>
     public RaiseDrop IsCorrectionDropRelayRaised { get; set; }
     [Column(TypeName = "timestamp without time zone")]
     public DateTime? DroppedAt { get; set; }
