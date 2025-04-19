b2016c280c2fc1154faba9137ca8fa1189c31ec5
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-04-20 00:43:39 +0900

    転てつ器処理のリファクタ

--- Traincrew_MultiATS_Server/Repositories/SwitchingMachineRoute/SwitchingMachineRouteRepository.cs
+++ Traincrew_MultiATS_Server/Repositories/SwitchingMachineRoute/SwitchingMachineRouteRepository.cs
@@ -3,10 +3,24 @@ using Traincrew_MultiATS_Server.Data;
 
 namespace Traincrew_MultiATS_Server.Repositories.SwitchingMachineRoute;
 
-public class SwitchingMachineRouteRepository(ApplicationDbContext context) : ISwitchingMachineRouteRepository
+public class SwitchingMachineRouteRepository : ISwitchingMachineRouteRepository
 {
+    private readonly ApplicationDbContext _context;
+
+    public SwitchingMachineRouteRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
     public Task<List<Models.SwitchingMachineRoute>> GetAll()
     {
-        return context.SwitchingMachineRoutes.ToListAsync();
+        return _context.SwitchingMachineRoutes.ToListAsync();
+    }
+
+    public async Task<List<Models.SwitchingMachineRoute>> GetBySwitchingMachineIds(List<ulong> switchingMachineIds)
+    {
+        return await _context.SwitchingMachineRoutes
+            .Where(route => switchingMachineIds.Contains(route.SwitchingMachineId))
+            .ToListAsync();
     }
 }
