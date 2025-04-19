cb92c7e175725f3f7a5e0fbf9fecc328512d0673
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-04-20 00:43:39 +0900

    転てつ器処理のリファクタ

--- Traincrew_MultiATS_Server/Repositories/SwitchingMachine/ISwitchingMachineRepository.cs
+++ Traincrew_MultiATS_Server/Repositories/SwitchingMachine/ISwitchingMachineRepository.cs
@@ -1,10 +1,24 @@
 namespace Traincrew_MultiATS_Server.Repositories.SwitchingMachine;
-using Datetime = DateTime;
 
 public interface ISwitchingMachineRepository
 {
+    /// <summary>
+    /// 転てつ器とその状態を取得する
+    /// </summary>
     Task<List<Models.SwitchingMachine>> GetSwitchingMachinesWithState();
-    Task<List<Models.SwitchingMachine>> GetSwitchingMachinesByIdsWithState(IEnumerable<ulong> ids);
-    Task UpdateSwitchEndTime(IEnumerable<ulong> ids, Datetime switchEndTime);
-    Task UpdateIsReverse(IEnumerable<ulong> ids, bool isReverse);
+
+    /// <summary>
+    /// 転換中の転てつ器のIDを取得する
+    /// </summary>
+    Task<List<ulong>> GetIdsWhereMoving();
+
+    /// <summary>
+    /// 単独てこが倒れている転てつ器のIDを取得する
+    /// </summary>
+    Task<List<ulong>> GetIdsWhereLeverReversed();
+
+    /// <summary>
+    /// てこリレー回路が扛上している進路に対する転てつ器のIDを取得する
+    /// </summary>
+    Task<List<ulong>> GetIdsWhereLeverRelayRaised();
 }
