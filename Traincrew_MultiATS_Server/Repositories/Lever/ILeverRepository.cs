b95049536abfe965ff811230209ee4f5ec4ef432
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-04-20 00:43:39 +0900

    転てつ器処理のリファクタ

--- Traincrew_MultiATS_Server/Repositories/Lever/ILeverRepository.cs
+++ Traincrew_MultiATS_Server/Repositories/Lever/ILeverRepository.cs
@@ -2,6 +2,8 @@ namespace Traincrew_MultiATS_Server.Repositories.Lever;
 
 public interface ILeverRepository
 {
+    Task<List<ulong>> GetIdsBySwitchingMachineIds(List<ulong> ids);
+
     Task<Models.Lever?> GetLeverByNameWithState(string name);
 
     Task<List<ulong>?> GetAllIds();
