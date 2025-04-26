bd49cadf0e0d5966323176aa4e1c38f3b232d861
TakumiteTudo <takumit2886@gmail.com>
2025-04-27 06:41:39 +0900

    開放てこのリポジトリ生成

---
+++ Traincrew_MultiATS_Server/Repositories/DirectionSelfControlLever/DirectionSelfControlLever.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Traincrew_MultiATS_Server.Data;
+
+namespace Traincrew_MultiATS_Server.Repositories.DirectionSelfControlLever;
+
+public class DirectionSelfControlLeverRepository(ApplicationDbContext context) : IDirectionSelfControlLeverRepository
+{
+    /// <summary>
+    /// 開放てこを名前から取得する。
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public async Task<Models.DirectionSelfControlLever?> GetDirectionSelfControlLeverByNameWithState(string name)
+    {
+        return await context.DirectionSelfControlLevers
+            .Include(lever => lever.DirectionSelfControlLeverState)
+            .FirstOrDefaultAsync(lever => lever.Name == name);
+    }
+}
---
+++ Traincrew_MultiATS_Server/Repositories/DirectionSelfControlLever/IDirectionSelfControlLeverRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Traincrew_MultiATS_Server.Repositories.DirectionSelfControlLever;
+
+public interface IDirectionSelfControlLeverRepository
+{
+    /// <summary>
+    /// 開放てこを名前から取得する。
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    Task<Models.DirectionSelfControlLever?> GetDirectionSelfControlLeverByNameWithState(string name);
+}
