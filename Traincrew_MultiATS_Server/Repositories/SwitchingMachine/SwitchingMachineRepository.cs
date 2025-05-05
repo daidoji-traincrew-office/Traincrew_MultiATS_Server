21927bf5e1a3acabad1ad41f0f17c84366b512bb
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-05-05 09:40:36 +0900

    各条件に想定方向と違うという条件を追加

--- Traincrew_MultiATS_Server/Repositories/SwitchingMachine/SwitchingMachineRepository.cs
+++ Traincrew_MultiATS_Server/Repositories/SwitchingMachine/SwitchingMachineRepository.cs
@@ -20,13 +20,17 @@ public class SwitchingMachineRepository(ApplicationDbContext context) : ISwitchi
             .Select(swm => swm.Id)
             .ToListAsync();
     }
-    
+
     public async Task<List<ulong>> GetIdsWhereLeverReversed()
     {
         // 2. 転てつ器の単独てこが倒れている転てつ器
-        return await context.Levers
-            .Where(l => l.SwitchingMachineId != null && l.LeverState.IsReversed != LCR.Center)
-            .Select(l => l.SwitchingMachineId.Value)
+        return await context.SwitchingMachines
+            .Include(swm => swm.SwitchingMachineState)
+            .Join(context.Levers, l => l.Id, swm => swm.Id, ( swm, l) => new { swm, l })
+            .Include(obj => obj.l.LeverState)
+            .Where(obj => obj.l.LeverState.IsReversed == LCR.Left && obj.swm.SwitchingMachineState.IsReverse != NR.Normal
+                || obj.l.LeverState.IsReversed == LCR.Right && obj.swm.SwitchingMachineState.IsReverse != NR.Reversed)
+            .Select(obj => obj.swm.Id)
             .ToListAsync();
     }
 
@@ -36,7 +40,11 @@ public class SwitchingMachineRepository(ApplicationDbContext context) : ISwitchi
         return await context.Routes
             .Include(r => r.RouteState)
             .Where(r => r.RouteState.IsLeverRelayRaised == RaiseDrop.Raise)
-            .Join(context.SwitchingMachineRoutes, r => r.Id, smr => smr.RouteId, (r, smr) => smr.SwitchingMachineId)
+            .Join(context.SwitchingMachineRoutes, r => r.Id, smr => smr.RouteId, (_, smr) => smr)
+            .Join(context.SwitchingMachines, smr => smr.SwitchingMachineId, sm => sm.Id, (smr, sm) => new { smr, sm })
+            .Include(s => s.sm.SwitchingMachineState)
+            .Where(s => s.smr.IsReverse != s.sm.SwitchingMachineState.IsReverse)
+            .Select(s => s.smr.SwitchingMachineId)
             .ToListAsync();
     }
 }
