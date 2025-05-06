4380418abc52dc8d69a5bf46a72a884ed30154a7
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-05-06 19:45:06 +0900

    lcrのPG定義削除

--- Traincrew_MultiATS_Server.Common/Models/LCR.cs
+++ Traincrew_MultiATS_Server.Common/Models/LCR.cs
@@ -2,11 +2,8 @@ namespace Traincrew_MultiATS_Server.Common.Models;
 // ReSharper disable once InconsistentNaming
 public enum LCR
 {
-   [PgName("left")]
    Left,
-   [PgName("center")]
    Center,
-   [PgName("right")]
    Right,
 }
     
