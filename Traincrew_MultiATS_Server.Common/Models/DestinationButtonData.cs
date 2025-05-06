66f79471181bf7498b91ef3ac1598cd48eb27895
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-05-06 20:00:21 +0900

    Interlocking統合召喚

--- Traincrew_MultiATS_Server.Common/Models/DestinationButtonData.cs
+++
@@ -1,8 +0,0 @@
-namespace Traincrew_MultiATS_Server.Common.Models;
-
-public class DestinationButtonData
-{
-    public string Name { get; init; }
-    public RaiseDrop IsRaised { get; set; }
-    public DateTime OperatedAt { get; set; }
-}
