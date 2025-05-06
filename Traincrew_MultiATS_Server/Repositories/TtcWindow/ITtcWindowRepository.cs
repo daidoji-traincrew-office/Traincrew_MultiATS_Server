98f79ae110dbeeaf207177ea6cc7b38403103418
TakumiteTudo <takumit2886@gmail.com>
2025-05-06 20:00:28 +0900

    対応リポジトリ生やした

---
+++ Traincrew_MultiATS_Server/Repositories/TtcWindow/ITtcWindowRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Traincrew_MultiATS_Server.Repositories.TtcWindow
+{
+    public interface ITtcWindowRepository
+    {
+        Task<List<Models.TtcWindow>> GetAllTtcWindow();
+        Task<List<Models.TtcWindow>> GetTtcWindowByName(List<string> name);
+        Task<List<Models.TtcWindowTrackCircuit>> GetWindowTrackCircuits();
+        Task<List<Models.TtcWindowTrackCircuit>> ttcWindowTrackCircuitsById(List<string> ttcWindowName);
+    }
+}
---
+++ Traincrew_MultiATS_Server/Repositories/TtcWindow/TtcWindowRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Traincrew_MultiATS_Server.Data;
+using Traincrew_MultiATS_Server.Models;
+
+namespace Traincrew_MultiATS_Server.Repositories.TtcWindow;
+
+public class TtcWindowRepository(ApplicationDbContext context) : ITtcWindowRepository
+{
+    public async Task<List<Models.TtcWindow>> GetAllTtcWindow()
+    {
+        return await context.TtcWindows
+            .ToListAsync();
+    }
+    public async Task<List<Models.TtcWindow>> GetTtcWindowByName(List<string> ttcWindowNames)
+    {
+        return await context.TtcWindows
+            .Where(obj => ttcWindowNames.Contains(obj.Name))
+            .ToListAsync();
+    }
+    public async Task<List<Models.TtcWindowTrackCircuit>> GetWindowTrackCircuits()
+    {
+        return await context.TtcWindowTrackCircuits
+            .ToListAsync();
+    }
+    public async Task<List<Models.TtcWindowTrackCircuit>> ttcWindowTrackCircuitsById(List<string> ttcWindowName)
+    {
+        return await context.TtcWindowTrackCircuits
+            .Where(obj => ttcWindowName.Contains(obj.TtcWindowName))
+            .ToListAsync();
+    }
+}
