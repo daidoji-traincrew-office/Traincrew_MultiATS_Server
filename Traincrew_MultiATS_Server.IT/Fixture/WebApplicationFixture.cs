16513d7f1b028d95f0df5d28002cd6f3a076f3bc
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-05-13 15:11:38 +0900

    factoryはメンバ変数としてもたせるように修正

--- Traincrew_MultiATS_Server.IT/Fixture/WebApplicationFixture.cs
+++ Traincrew_MultiATS_Server.IT/Fixture/WebApplicationFixture.cs
@@ -5,12 +5,14 @@ using TypedSignalR.Client;
 
 namespace Traincrew_MultiATS_Server.IT.Fixture;
 
-public class WebApplicationFixture : WebApplicationFactory<Program>
+public class WebApplicationFixture
 {
     private const string TrainHubPath = "hub/train";
     private const string TIDHubPath = "hub/TID";
     private const string InterlockingHubPath = "hub/interlocking";
     private const string CommanderTableHubPath = "hub/commander_table";
+    
+    private WebApplicationFactory<Program> factory = new();
 
     /// <summary>
     /// ITrainHubContractとITrainClientContract用のHubConnectionを生成し、両方を返します。
@@ -20,8 +22,8 @@ public class WebApplicationFixture : WebApplicationFactory<Program>
     public (HubConnection, ITrainHubContract) CreateTrainHub(ITrainClientContract? receiver = null)
     {
         var connection = new HubConnectionBuilder()
-            .WithUrl(Server.BaseAddress + TrainHubPath,
-                o => { o.HttpMessageHandlerFactory = _ => Server.CreateHandler(); })
+            .WithUrl(factory.Server.BaseAddress + TrainHubPath,
+                o => { o.HttpMessageHandlerFactory = _ => factory.Server.CreateHandler(); })
             .Build();
 
         var hubContract = connection.CreateHubProxy<ITrainHubContract>();
@@ -41,8 +43,8 @@ public class WebApplicationFixture : WebApplicationFactory<Program>
     public (HubConnection, ITIDHubContract) CreateTIDHub(ITIDClientContract? receiver = null)
     {
         var connection = new HubConnectionBuilder()
-            .WithUrl(Server.BaseAddress + TIDHubPath,
-                o => { o.HttpMessageHandlerFactory = _ => Server.CreateHandler(); })
+            .WithUrl(factory.Server.BaseAddress + TIDHubPath,
+                o => { o.HttpMessageHandlerFactory = _ => factory.Server.CreateHandler(); })
             .Build();
 
         var hubContract = connection.CreateHubProxy<ITIDHubContract>();
@@ -62,8 +64,8 @@ public class WebApplicationFixture : WebApplicationFactory<Program>
     public (HubConnection, IInterlockingHubContract) CreateInterlockingHub(IInterlockingClientContract? receiver = null)
     {
         var connection = new HubConnectionBuilder()
-            .WithUrl(Server.BaseAddress + InterlockingHubPath,
-                o => { o.HttpMessageHandlerFactory = _ => Server.CreateHandler(); })
+            .WithUrl(factory.Server.BaseAddress + InterlockingHubPath,
+                o => { o.HttpMessageHandlerFactory = _ => factory.Server.CreateHandler(); })
             .Build();
 
         var hubContract = connection.CreateHubProxy<IInterlockingHubContract>();
@@ -84,8 +86,8 @@ public class WebApplicationFixture : WebApplicationFactory<Program>
         ICommanderTableClientContract? receiver = null)
     {
         var connection = new HubConnectionBuilder()
-            .WithUrl(Server.BaseAddress + CommanderTableHubPath,
-                o => { o.HttpMessageHandlerFactory = _ => Server.CreateHandler(); })
+            .WithUrl(factory.Server.BaseAddress + CommanderTableHubPath,
+                o => { o.HttpMessageHandlerFactory = _ => factory.Server.CreateHandler(); })
             .Build();
 
         var hubContract = connection.CreateHubProxy<ICommanderTableHubContract>();
