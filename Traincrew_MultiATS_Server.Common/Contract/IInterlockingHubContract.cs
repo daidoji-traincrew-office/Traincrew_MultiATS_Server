65d12473bc05cc335ba9c82168af92bb15406e97
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-05-18 02:51:25 +0900

    呼び出し型定時処理を削除

--- Traincrew_MultiATS_Server.Common/Contract/IInterlockingHubContract.cs
+++ Traincrew_MultiATS_Server.Common/Contract/IInterlockingHubContract.cs
@@ -4,7 +4,6 @@ namespace Traincrew_MultiATS_Server.Common.Contract;
 
 public interface IInterlockingHubContract
 {
-    Task<DataToInterlocking> SendData_Interlocking(List<string> activeStationsList);
     Task SetPhysicalLeverData(InterlockingLeverData leverData);
     Task<bool> SetPhysicalKeyLeverData(InterlockingKeyLeverData keyLeverData);
     Task SetDestinationButtonState(DestinationButtonData buttonData);
--- Traincrew_MultiATS_Server/Hubs/InterlockingHub.cs
+++ Traincrew_MultiATS_Server/Hubs/InterlockingHub.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.SignalR;
 using OpenIddict.Validation.AspNetCore;
 using Traincrew_MultiATS_Server.Common.Contract;
 using Traincrew_MultiATS_Server.Common.Models;
-using Traincrew_MultiATS_Server.Models;
 using Traincrew_MultiATS_Server.Services;
 using static OpenIddict.Abstractions.OpenIddictConstants;
 
@@ -15,69 +14,8 @@ namespace Traincrew_MultiATS_Server.Hubs;
     Policy = "InterlockingPolicy"
 )]
 public class InterlockingHub(
-    TrackCircuitService trackCircuitService,
-    SignalService signalService,
-    StationService stationService,
     InterlockingService interlockingService) : Hub<IInterlockingClientContract>, IInterlockingHubContract
 {
-    public async Task<DataToInterlocking> SendData_Interlocking(List<string> activeStationsList)
-    {
-        // Todo: クライアントごとに取得処理をするのではなく、サーバーで定時で取得処理をして、必要なクライアントに配る形式にする
-        // Todo: めんどいし、Interlocking全取得してOfTypeと変換作って動くようにする    
-        var allInterlockingObjects = await interlockingService.GetInterlockingObjects();
-        var destinationButtons = await interlockingService.GetDestinationButtonsByStationIds(activeStationsList);
-        // List<string> clientData.ActiveStationsListの駅IDから、指定された駅にある信号機名称をList<string>で返すやつ
-        var signalNames = await signalService.GetSignalNamesByStationIds(activeStationsList);
-        // それら全部の信号の現示計算
-        var signalIndications = await signalService.CalcSignalIndication(signalNames);
-        var lamps = await interlockingService.GetLamps(activeStationsList);
-
-        var response = new DataToInterlocking
-        {
-            TrackCircuits = allInterlockingObjects
-                .OfType<TrackCircuit>()
-                .Select(TrackCircuitService.ToTrackCircuitData)
-                .ToList(),
-
-            Points = allInterlockingObjects
-                .OfType<SwitchingMachine>()
-                .Select(SwitchingMachineService.ToSwitchData)
-                .ToList(),
-
-            // Todo: 方向てこのほうのリストを連結する
-            PhysicalLevers = allInterlockingObjects
-                .OfType<Lever>()
-                .Select(InterlockingService.ToLeverData)
-                .ToList(),
-
-            // Todo: 駅扱てこの実装と両方渡し
-            PhysicalKeyLevers = allInterlockingObjects
-                .OfType<DirectionSelfControlLever>()
-                .Select(InterlockingService.ToKeyLeverData)
-                .ToList(),
-
-            PhysicalButtons = destinationButtons
-                .Select(button => InterlockingService.ToDestinationButtonData(button.DestinationButtonState))
-                .ToList(),
-
-            Directions = allInterlockingObjects
-                .OfType<DirectionRoute>()
-                .Select(InterlockingService.ToDirectionData)
-                .ToList(),
-
-            // Todo: 列番表示の実装から
-            Retsubans = new List<InterlockingRetsubanData>(),
-
-            // 各ランプの状態 
-            Lamps = lamps,
-
-            Signals = signalIndications
-                .Select(pair => SignalService.ToSignalData(pair.Key, pair.Value))
-                .ToList()
-        };
-        return response;
-    }
-
     public async Task SetPhysicalLeverData(InterlockingLeverData leverData)
     {
         await interlockingService.SetPhysicalLeverData(leverData);
