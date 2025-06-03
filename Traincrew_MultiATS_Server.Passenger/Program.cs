fcaafb1415f99a68a83a1bb0e5cce765555f303c
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-06-03 13:32:12 +0900

    DI

--- Traincrew_MultiATS_Server.Passenger/Program.cs
+++ Traincrew_MultiATS_Server.Passenger/Program.cs
@@ -1,3 +1,5 @@
+using Traincrew_MultiATS_Server.Passenger.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+
+// DI
+builder.Services.AddScoped<PassengerService>();
 
 var app = builder.Build();
 app.MapControllers();
