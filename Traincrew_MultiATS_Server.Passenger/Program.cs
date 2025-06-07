bde3481ca31425ebe20013d58fadf52e5942c7c7
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-06-07 18:37:19 +0900

    Program.cs リファクタ

--- Traincrew_MultiATS_Server.Passenger/Program.cs
+++ Traincrew_MultiATS_Server.Passenger/Program.cs
@@ -1,30 +1,56 @@
 using Traincrew_MultiATS_Server.Passenger.Service;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace Traincrew_MultiATS_Server.Passenger;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
-
-// Todo: CORS設定, Rate Limit設定, CSP設定
-// Todo: (優先度低)キャッシュ制御, Response Compression
-
-// DI
-builder.Services.AddScoped<PassengerService>();
-
-var app = builder.Build();
-app.MapControllers();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var isDevelopment = builder.Environment.IsDevelopment();
+
+        ConfigureServices(builder, isDevelopment);
+
+        var app = builder.Build();
+
+        await Configure(app, isDevelopment);
+
+        await app.RunAsync();
+    }
+
+    private static void ConfigureServices(WebApplicationBuilder builder, bool isDevelopment)
+    {
+        // Controller
+        builder.Services.AddControllers();
+
+        // Swagger
+        if (isDevelopment)
+        {
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+        }
+
+        // Todo: CORS設定, Rate Limit設定, CSP設定
+        // Todo: (優先度低)キャッシュ制御, Response Compression
+
+        // DI
+        builder.Services.AddScoped<PassengerService>();
+    }
+
+    private static async Task Configure(WebApplication app, bool isDevelopment)
+    {
+        if (isDevelopment)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHsts();
+        app.UseHttpsRedirection();
+
+        app.MapControllers();
+
+        await Task.CompletedTask;
+    }
 }
 
-app.UseHsts();
-app.UseHttpsRedirection();
-
-app.Run();
