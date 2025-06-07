b7a56fdee39f26f11d3a48c1b8f8386828dc78e6
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-06-07 18:26:58 +0900

    Server側のTrackCircuitServiceに依存してみよう

--- Traincrew_MultiATS_Server.Passenger/Service/PassengerService.cs
+++ Traincrew_MultiATS_Server.Passenger/Service/PassengerService.cs
@@ -1,6 +1,8 @@
+using Traincrew_MultiATS_Server.Services;
+
 namespace Traincrew_MultiATS_Server.Passenger.Service;
 
-public class PassengerService
+public class PassengerService(TrackCircuitService trackCircuitService)
 {
-    
+          
 }
