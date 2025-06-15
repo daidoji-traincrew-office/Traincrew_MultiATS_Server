de9bec11f0c58c78fe596acb97dc86b8f750320c
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-06-15 22:05:37 +0900

    DI追加

--- Traincrew_MultiATS_Server.Passenger/Program.cs
+++ Traincrew_MultiATS_Server.Passenger/Program.cs
@@ -1,8 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using Npgsql;
 using Traincrew_MultiATS_Server.Data;
+using Traincrew_MultiATS_Server.Repositories.Datetime;
 using Traincrew_MultiATS_Server.Repositories.General;
+using Traincrew_MultiATS_Server.Repositories.NextSignal;
+using Traincrew_MultiATS_Server.Repositories.OperationNotification;
+using Traincrew_MultiATS_Server.Repositories.Protection;
+using Traincrew_MultiATS_Server.Repositories.Route;
+using Traincrew_MultiATS_Server.Repositories.Signal;
+using Traincrew_MultiATS_Server.Repositories.SignalRoute;
 using Traincrew_MultiATS_Server.Repositories.TrackCircuit;
+using Traincrew_MultiATS_Server.Repositories.Train;
+using Traincrew_MultiATS_Server.Repositories.TrainCar;
+using Traincrew_MultiATS_Server.Repositories.TrainDiagram;
 using Traincrew_MultiATS_Server.Services;
 
 namespace Traincrew_MultiATS_Server.Passenger;
@@ -45,7 +55,22 @@ public class Program
         builder.Services
             .AddScoped<IGeneralRepository, GeneralRepository>()
             .AddScoped<ITrackCircuitRepository, TrackCircuitRepository>()
+            .AddScoped<ITrainRepository, TrainRepository>()
+            .AddScoped<ITrainCarRepository, TrainCarRepository>()
+            .AddScoped<ITrainDiagramRepository, TrainDiagramRepository>()
+            .AddScoped<INextSignalRepository, NextSignalRepository>()
+            .AddScoped<ISignalRepository, SignalRepository>()
+            .AddScoped<IOperationNotificationRepository, OperationNotificationRepository>()
+            .AddScoped<IProtectionRepository, ProtectionRepository>()
+            .AddScoped<IRouteRepository, RouteRepository>()
+            .AddScoped<ISignalRouteRepository, SignalRouteRepository>()
+            .AddScoped<IDateTimeRepository, DateTimeRepository>()
             .AddScoped<TrackCircuitService>()
+            .AddScoped<SignalService>()
+            .AddScoped<OperationNotificationService>()
+            .AddScoped<ProtectionService>()
+            .AddScoped<RouteService>()
+            .AddScoped<TrainService>()
             .AddScoped<PassengerService>();
     }
 
