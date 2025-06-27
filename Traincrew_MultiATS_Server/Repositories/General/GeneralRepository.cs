bdce602d00ea3c0498848b1b61fd9497d078bfa4
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-06-28 03:11:18 +0900

    防護State編集を作成

--- Traincrew_MultiATS_Server/Repositories/General/GeneralRepository.cs
+++ Traincrew_MultiATS_Server/Repositories/General/GeneralRepository.cs
@@ -5,6 +5,26 @@ namespace Traincrew_MultiATS_Server.Repositories.General;
 
 public class GeneralRepository(ApplicationDbContext context) : IGeneralRepository
 {
+    public async Task Add<T>(T entity)
+    {
+        context.Add(entity);
+        await context.SaveChangesAsync();
+        context.Entry(entity).State = EntityState.Detached;
+    }
+
+    public async Task AddAll<T>(IEnumerable<T> entities)
+    {
+        foreach (var entity in entities)
+        {
+            context.Add(entity);
+        }
+        await context.SaveChangesAsync();
+        foreach (var entity in entities)
+        {
+            context.Entry(entity).State = EntityState.Detached;
+        }
+    }
+
     public async Task Save<T>(T entity)
     {
         context.Update(entity);
