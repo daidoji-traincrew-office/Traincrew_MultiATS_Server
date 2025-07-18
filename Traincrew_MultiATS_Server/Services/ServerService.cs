e0ee18f6b62ea2e267ca5acb661db81f23803a91
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-07-18 16:22:04 +0900

    ロックを取っておく

--- Traincrew_MultiATS_Server/Services/ServerService.cs
+++ Traincrew_MultiATS_Server/Services/ServerService.cs
@@ -1,13 +1,18 @@
 using Traincrew_MultiATS_Server.Common.Models;
+using Traincrew_MultiATS_Server.Repositories.Mutex;
 using Traincrew_MultiATS_Server.Repositories.Server;
 using Traincrew_MultiATS_Server.Scheduler;
 
 namespace Traincrew_MultiATS_Server.Services;
 
-public class ServerService(IServerRepository serverRepository, SchedulerManager schedulerManager)
+public class ServerService(
+    IServerRepository serverRepository, 
+    SchedulerManager schedulerManager,
+    IMutexRepository mutexRepository)
 {
     public async Task<ServerMode> GetServerStateAsync()
     {
+        await using var mutex = await mutexRepository.AcquireAsync(nameof(ServerService));
         var state = await serverRepository.GetServerStateAsync();
         if (state == null)
         {
@@ -18,11 +23,18 @@ public class ServerService(IServerRepository serverRepository, SchedulerManager 
 
     public async Task SetServerStateAsync(ServerMode mode)
     {
+        await using var mutex = await mutexRepository.AcquireAsync(nameof(ServerService));
         await serverRepository.SetServerStateAsync(mode);
         await UpdateSchedulerAsync();
     }
 
     public async Task UpdateSchedulerAsync()
+    {
+        await using var mutex = await mutexRepository.AcquireAsync(nameof(ServerService));
+        await UpdateSchedulerAsyncWithoutLock();
+    }
+
+    private async Task UpdateSchedulerAsyncWithoutLock()
     {
         var mode = await GetServerStateAsync();
         if (mode == ServerMode.Off)
