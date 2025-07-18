89ad9604ebf3cc760dd642f0155443f38e40b2f9
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-07-18 15:36:56 +0900

    テーブル名定義

--- Traincrew_MultiATS_Server/Models/ServerState.cs
+++ Traincrew_MultiATS_Server/Models/ServerState.cs
@@ -1,7 +1,9 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Traincrew_MultiATS_Server.Common.Models;
 
 namespace Traincrew_MultiATS_Server.Models;
 
+[Table("server_state")]
 public class ServerState
 {
     public int Id { get; set; }
