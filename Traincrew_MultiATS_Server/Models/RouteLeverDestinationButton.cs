1d22afff07e75c8605ffbf8a57a14b572f8ee5ab
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-08-06 09:46:38 +0900

    てこリレー大幅リファクタ

--- Traincrew_MultiATS_Server/Models/RouteLeverDestinationButton.cs
+++ Traincrew_MultiATS_Server/Models/RouteLeverDestinationButton.cs
@@ -10,7 +10,8 @@ public class RouteLeverDestinationButton
     public ulong Id { get; set; }
     public ulong RouteId { get; set; }
     public ulong LeverId { get; set; }
+    public Lever Lever { get; set; }
     public LR Direction { get; set; }
     public string? DestinationButtonName { get; set; }
-    
+    public DestinationButton? DestinationButton { get; set; }
 }
