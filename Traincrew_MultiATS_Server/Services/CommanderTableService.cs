3ec686cca182225c72ab865515d47d96ceabe3a0
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-08-23 15:28:42 +0900

    Add忘れ

---
+++ Traincrew_MultiATS_Server/Services/CommanderTableService.cs
@@ -0,0 +1,27 @@
+using Traincrew_MultiATS_Server.Common.Models;
+
+namespace Traincrew_MultiATS_Server.Services;
+
+public class CommanderTableService(
+    TrackCircuitService trackCircuitService,
+    OperationNotificationService operationNotificationService,
+    OperationInformationService operationInformationService,
+    ProtectionService protectionService,
+    ServerService serverService,
+    TrainService trainService
+)
+{
+    public async Task<DataToCommanderTable> SendData_CommanderTable()
+    {
+        return new()
+        {
+            TroubleDataList = [],
+            OperationNotificationDataList = await operationNotificationService.GetOperationNotificationData(),
+            TrackCircuitDataList = await trackCircuitService.GetAllTrackCircuitDataList(),
+            OperationInformationDataList = await operationInformationService.GetAllOperationInformations(),
+            ProtectionZoneDataList = await protectionService.GetProtectionZoneStates(),
+            TrainStateDataList = await trainService.GetAllTrainState(),
+            ServerMode = await serverService.GetServerModeAsync()
+        };
+    }
+}
