6dbad6670038ef9a0b596bfe9aa99f454f6e18b9
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-09-09 19:37:34 +0900

    余計なIncludeを削除

--- Traincrew_MultiATS_Server/Repositories/SwitchingMachine/SwitchingMachineRepository.cs
+++ Traincrew_MultiATS_Server/Repositories/SwitchingMachine/SwitchingMachineRepository.cs
@@ -26,9 +26,7 @@ public class SwitchingMachineRepository(ApplicationDbContext context) : ISwitchi
     {
         // 2. 転てつ器の単独てこが倒れている転てつ器
         return await context.SwitchingMachines
-            .Include(swm => swm.SwitchingMachineState)
             .Join(context.Levers, l => l.Id, swm => swm.Id, ( swm, l) => new { swm, l })
-            .Include(obj => obj.l.LeverState)
             .Where(obj => obj.l.LeverState.IsReversed == LCR.Left && obj.swm.SwitchingMachineState.IsReverse != NR.Normal
                 || obj.l.LeverState.IsReversed == LCR.Right && obj.swm.SwitchingMachineState.IsReverse != NR.Reversed)
             .Select(obj => obj.swm.Id)
@@ -39,11 +37,9 @@ public class SwitchingMachineRepository(ApplicationDbContext context) : ISwitchi
     {
         // 3. てこリレー回路が上がっている進路に対する転てつ器
         return await context.Routes
-            .Include(r => r.RouteState)
             .Where(r => r.RouteState.IsLeverRelayRaised == RaiseDrop.Raise)
             .Join(context.SwitchingMachineRoutes, r => r.Id, smr => smr.RouteId, (_, smr) => smr)
             .Join(context.SwitchingMachines, smr => smr.SwitchingMachineId, sm => sm.Id, (smr, sm) => new { smr, sm })
-            .Include(s => s.sm.SwitchingMachineState)
             .Where(s => s.smr.IsReverse != s.sm.SwitchingMachineState.IsReverse)
             .Select(s => s.smr.SwitchingMachineId)
             .ToListAsync();
