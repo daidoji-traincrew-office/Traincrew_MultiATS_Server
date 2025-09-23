bbd1f017873fb32422ef5c6c9ae3e3737d6491ed
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-09-23 22:34:54 +0900

    コメント修正

--- Traincrew_MultiATS_Server/Services/RendoService.cs
+++ Traincrew_MultiATS_Server/Services/RendoService.cs
@@ -1365,9 +1365,12 @@ public class RendoService(
         {
             return RaiseDrop.Drop;
         }
-
-        // 総括制御される進路のいずれかが信号制御リレーが落下している場合、信号制御リレーを落下させる
-        if (targetThrowOutRoutes.Any(r => r.RouteState.IsThrowOutXRRelayRaised == RaiseDrop.Raise) && targetThrowOutRoutes.All(r => r.RouteState.IsSignalControlRaised == RaiseDrop.Drop))
+        
+        // 総括制御する場合、総括制御先のすべての信号制御リレーが落下している場合、信号制御リレーを落下させる
+        // (= すなわち、総括制御の場合は奥の信号が開いてないなら開けない)
+        if (
+            targetThrowOutRoutes.Any(r => r.RouteState.IsThrowOutXRRelayRaised == RaiseDrop.Raise) 
+            && targetThrowOutRoutes.All(r => r.RouteState.IsSignalControlRaised == RaiseDrop.Drop))
         {
             return RaiseDrop.Drop;
         }
