a37584ca7f517d99cbddf9f12992dcf2ce5a79df
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-10-05 01:54:41 +0900

    xリレー、sリレー追加

--- Database/schema.sql
+++ Database/schema.sql
@@ -494,8 +494,10 @@ CREATE TABLE route_state
     is_approach_lock_mr_raised                      raise_drop NOT NULL,                      -- 接近鎖状が上がっているか
     is_approach_lock_ms_raised                      raise_drop NOT NULL,                      -- 接近鎖状が上がっているか
     is_route_lock_raised                            raise_drop NOT NULL,                      -- 進路鎖状が上がっているか
-    is_throw_out_xr_relay_raised                    raise_drop NOT NULL,                      -- 統括制御リレーが上がっているか
-    is_throw_out_ys_relay_raised                    raise_drop NOT NULL                       -- 統括制御リレーが上がっているか
+    is_throw_out_xr_relay_raised                    raise_drop NOT NULL,                      -- xrリレーが上がっているか
+    is_throw_out_ys_relay_raised                    raise_drop NOT NULL,                      -- ysリレーが上がっているか
+    is_throw_out_x_relay_raised                     raise_drop NOT NULL DEFAULT 'drop',       -- xリレーが上がっているか
+    is_throw_out_s_relay_raised                     raise_drop NOT NULL DEFAULT 'drop'        -- sリレーが上がっているか
 );
 
 -- 信号機状態
