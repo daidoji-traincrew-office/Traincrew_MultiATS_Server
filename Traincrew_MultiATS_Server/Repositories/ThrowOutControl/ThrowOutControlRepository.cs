84cf7d5b18ec97dca2afff420f1b2155d844bcaa
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-10-06 23:34:17 +0900

    メソッド追加

--- Traincrew_MultiATS_Server/Repositories/ThrowOutControl/ThrowOutControlRepository.cs
+++ Traincrew_MultiATS_Server/Repositories/ThrowOutControl/ThrowOutControlRepository.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Traincrew_MultiATS_Server.Data;
+using Traincrew_MultiATS_Server.Models;
 
 namespace Traincrew_MultiATS_Server.Repositories.ThrowOutControl;
 
@@ -17,11 +18,40 @@ public class ThrowOutControlRepository(ApplicationDbContext context) : IThrowOut
             .Where(t => sourceIds.Contains(t.SourceId))
             .ToListAsync();
     }
-    
+
+    public async Task<List<Models.ThrowOutControl>> GetBySourceIds(List<ulong> sourceIds, List<ThrowOutControlType> controlTypes)
+    {
+        return await context.ThrowOutControls
+            .Where(t => sourceIds.Contains(t.SourceId) && controlTypes.Contains(t.ControlType))
+            .ToListAsync();
+    }
+
     public async Task<List<Models.ThrowOutControl>> GetByTargetIds(List<ulong> targetIds)
     {
         return await context.ThrowOutControls
             .Where(t => targetIds.Contains(t.TargetId))
+            .ToListAsync();
+    }
+
+    public async Task<List<Models.ThrowOutControl>> GetByTargetIds(List<ulong> targetIds, List<ThrowOutControlType> controlTypes)
+    {
+        return await context.ThrowOutControls
+            .Where(t => targetIds.Contains(t.TargetId) && controlTypes.Contains(t.ControlType))
+            .ToListAsync();
+    }
+
+    public async Task<List<Models.ThrowOutControl>> GetBySourceAndTargetIds(List<ulong> ids)
+    {
+        return await context.ThrowOutControls
+            .Where(t => ids.Contains(t.SourceId) || ids.Contains(t.TargetId))
+            .ToListAsync();
+
+    }
+
+    public async Task<List<Models.ThrowOutControl>> GetByControlTypes(List<ThrowOutControlType> controlTypes)
+    {
+        return await context.ThrowOutControls
+            .Where(t => controlTypes.Contains(t.ControlType))
             .ToListAsync();
     }
 }
