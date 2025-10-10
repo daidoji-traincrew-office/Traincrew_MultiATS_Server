bbbaad168bbd15a6ca1d294161e10a9cf49ff47f
TakumiteTudo <takumit2886@gmail.com>
2025-10-10 19:54:38 +0900

    現在のサーバー状態を旅客向け情報に含むように

--- Traincrew_MultiATS_Server/Services/PassengerService.cs
+++ Traincrew_MultiATS_Server/Services/PassengerService.cs
@@ -3,19 +3,22 @@ using Traincrew_MultiATS_Server.Common.Models;
 namespace Traincrew_MultiATS_Server.Services;
 
 public class PassengerService(
+    ServerService serverService,
     TrackCircuitService trackCircuitService,
     TrainService trainService,
     OperationInformationService operationInformationService
 )
 {
-    public async Task<ServerToPassengerData>  GetServerToPassengerData()
+    public async Task<ServerToPassengerData> GetServerToPassengerData()
     {
+        var serverMode = ServerMode.Public == await serverService.GetServerModeAsync();
         var trackCircuitData = await trackCircuitService.GetShortCircuitedTrackCircuitDataList();
         var trainInfoByTrainNumber = await trainService.GetTrainInfoGroupByTrainNumber();
         var operationInformations = await operationInformationService.GetOperationInformations();
 
         return new()
         {
+            ServerMode = serverMode,
             TrackCircuitData = trackCircuitData,
             TrainInfos = trainInfoByTrainNumber,
             OperationInformations = operationInformations
