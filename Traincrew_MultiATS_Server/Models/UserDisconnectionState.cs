0d7e2b938a24295a2ba0bd2e5a3db1b04c6a2cbc
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-11-22 19:59:13 +0900

    Key

--- Traincrew_MultiATS_Server/Models/UserDisconnectionState.cs
+++ Traincrew_MultiATS_Server/Models/UserDisconnectionState.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Traincrew_MultiATS_Server.Models;
@@ -5,5 +6,6 @@ namespace Traincrew_MultiATS_Server.Models;
 [Table("user_disconnection_state")]
 public class UserDisconnectionState
 {
+    [Key]
     public ulong UserId { get; set; }
 }
