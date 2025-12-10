251b1e429db3f0b159aa60557d78d155e1e3a2be
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2025-12-10 11:53:45 +0900

    信号盤の信号情報の定時処理も分離

--- Traincrew_MultiATS_Server.Common/Contract/IInterlockingHubContract.cs
+++ Traincrew_MultiATS_Server.Common/Contract/IInterlockingHubContract.cs
@@ -13,4 +13,5 @@ public interface IInterlockingHubContract
 public interface IInterlockingClientContract
 {
     Task ReceiveData(DataToInterlocking data);
+    Task ReceiveSignalData(List<SignalData> signalData);
 }
