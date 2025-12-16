7f1cea7f6598ca6e0fb3bb66b791d10f3244a640
TakumiteTudo <takumit2886@gmail.com>
2025-12-16 12:58:08 +0900

    内方侵入条件の改善誤りを修正

--- Traincrew_MultiATS_Server/Services/TtcStationControlService.cs
+++ Traincrew_MultiATS_Server/Services/TtcStationControlService.cs
@@ -215,7 +215,7 @@ public class TtcStationControlService(
             {
                 var routeState = routes.GetValueOrDefault(cond.RouteId)?.RouteState;
                 return routeState != null &&
-                       (routeState.IsRouteLockRaised == RaiseDrop.Drop || routeState.IsApproachLockMRRaised == RaiseDrop.Raise);
+                       (routeState.IsRouteLockRaised == RaiseDrop.Drop && routeState.IsApproachLockMRRaised == RaiseDrop.Raise);
             }))
             {
                 continue;
