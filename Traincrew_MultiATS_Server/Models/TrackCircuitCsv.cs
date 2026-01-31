a92157337fee9d923ddedf9fcb6fad8c6bb4b4b3
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2026-02-01 00:17:19 +0900

    列がズレてる！

--- Traincrew_MultiATS_Server/Models/TrackCircuitCsv.cs
+++ Traincrew_MultiATS_Server/Models/TrackCircuitCsv.cs
@@ -10,15 +10,15 @@ public class TrackCircuitCsv
     public required List<string> NextSignalNamesUp { get; set; } // 上り信号機1-5
     public required List<string> NextSignalNamesDown { get; set; } // 下り信号機1-5
     public int? ProtectionZone { get; set; } // 防護無線ゾーン
-    public string? TargetStation { get; set; } // 対象駅(列32)
-    public int? UpTimeElement6Car { get; set; } // 上り進出時素6連(列33)
-    public int? UpTimeElement4Car { get; set; } // 上り進出時素4連(列34)
-    public int? UpTimeElement2Car { get; set; } // 上り進出時素2連(列35)
-    public int? UpTimeElementPass { get; set; } // 上り進出時素通過(列36)
-    public int? DownTimeElement6Car { get; set; } // 下り進出時素6連(列37)
-    public int? DownTimeElement4Car { get; set; } // 下り進出時素4連(列38)
-    public int? DownTimeElement2Car { get; set; } // 下り進出時素2連(列39)
-    public int? DownTimeElementPass { get; set; } // 下り進出時素通過(列40)
+    public string? TargetStation { get; set; } // 対象駅(列35)
+    public int? UpTimeElement6Car { get; set; } // 上り進出時素6連(列36)
+    public int? UpTimeElement4Car { get; set; } // 上り進出時素4連(列37)
+    public int? UpTimeElement2Car { get; set; } // 上り進出時素2連(列38)
+    public int? UpTimeElementPass { get; set; } // 上り進出時素通過(列39)
+    public int? DownTimeElement6Car { get; set; } // 下り進出時素6連(列40)
+    public int? DownTimeElement4Car { get; set; } // 下り進出時素4連(列41)
+    public int? DownTimeElement2Car { get; set; } // 下り進出時素2連(列42)
+    public int? DownTimeElementPass { get; set; } // 下り進出時素通過(列43)
 }
 
 public sealed class TrackCircuitCsvMap : ClassMap<TrackCircuitCsv>
@@ -30,18 +30,18 @@ public sealed class TrackCircuitCsvMap : ClassMap<TrackCircuitCsv>
         Map(m => m.NextSignalNamesDown).Convert(GetNextSignalNamesDown);
         Map(m => m.ProtectionZone).Convert(GetProtectionZone);
 
-        // 列32: 対象駅
-        Map(m => m.TargetStation).Index(32).TypeConverter<EmptyStringToNullConverter>();
+        // 列35: 対象駅
+        Map(m => m.TargetStation).Index(34).TypeConverter<EmptyStringToNullConverter>();
 
-        // 列33-40: 時素値
-        Map(m => m.UpTimeElement6Car).Index(33).TypeConverter<EmptyStringToNullableIntConverter>();
-        Map(m => m.UpTimeElement4Car).Index(34).TypeConverter<EmptyStringToNullableIntConverter>();
-        Map(m => m.UpTimeElement2Car).Index(35).TypeConverter<EmptyStringToNullableIntConverter>();
-        Map(m => m.UpTimeElementPass).Index(36).TypeConverter<EmptyStringToNullableIntConverter>();
-        Map(m => m.DownTimeElement6Car).Index(37).TypeConverter<EmptyStringToNullableIntConverter>();
-        Map(m => m.DownTimeElement4Car).Index(38).TypeConverter<EmptyStringToNullableIntConverter>();
-        Map(m => m.DownTimeElement2Car).Index(39).TypeConverter<EmptyStringToNullableIntConverter>();
-        Map(m => m.DownTimeElementPass).Index(40).TypeConverter<EmptyStringToNullableIntConverter>();
+        // 列36-43: 時素値
+        Map(m => m.UpTimeElement6Car).Index(35).TypeConverter<EmptyStringToNullableIntConverter>();
+        Map(m => m.UpTimeElement4Car).Index(36).TypeConverter<EmptyStringToNullableIntConverter>();
+        Map(m => m.UpTimeElement2Car).Index(37).TypeConverter<EmptyStringToNullableIntConverter>();
+        Map(m => m.UpTimeElementPass).Index(38).TypeConverter<EmptyStringToNullableIntConverter>();
+        Map(m => m.DownTimeElement6Car).Index(39).TypeConverter<EmptyStringToNullableIntConverter>();
+        Map(m => m.DownTimeElement4Car).Index(40).TypeConverter<EmptyStringToNullableIntConverter>();
+        Map(m => m.DownTimeElement2Car).Index(41).TypeConverter<EmptyStringToNullableIntConverter>();
+        Map(m => m.DownTimeElementPass).Index(42).TypeConverter<EmptyStringToNullableIntConverter>();
     }
 
     private static List<string> GetNextSignalNamesUp(ConvertFromStringArgs row)
