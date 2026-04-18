37ff2cadd99e0bdc5f6ac00d75181d25b9428cb9
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2026-04-18 11:47:13 +0900

    add忘れ2

---
+++ Traincrew_MultiATS_Server/Models/DiagramJson.cs
@@ -0,0 +1,14 @@
+using System.Text.Json.Serialization;
+
+namespace Traincrew_MultiATS_Server.Models;
+
+public class DiagramJson
+{
+    public string Version { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+
+    [JsonPropertyName("time_range")]
+    public string TimeRange { get; set; } = string.Empty;
+
+    public List<TTC_Train> TrainList { get; set; } = [];
+}
